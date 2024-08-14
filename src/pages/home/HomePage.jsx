abc6feb1c39284c69437c2480e3c30220517260f
Pawan Chaukiyal <pawanchaukiyal@gmail.com>
2024-08-14 23:26:08 +0530

    loader remove from homepage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -5,7 +5,6 @@ import Category from '../../components/category/Category'
 import ProductCard from '../../components/ProductCard/ProductCard'
 import Track from '../../components/track/Track'
 import Testimonials from '../../components/testimonials/Testimonials'
-import Loader from '../../components/loader/Loader'
 
 
 const HomePage = () => {
@@ -17,7 +16,6 @@ const HomePage = () => {
       <ProductCard/>
       <Track/>
       <Testimonials/>
-      <Loader/>
     </Layout>
   )
 }
